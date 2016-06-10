18f1cce5fc7267154643876b9eaa2d14c5654e7c
Jethro Manalo <jethro_manalo@dlsu.edu.ph>
2016-06-10 20:12:37 +0800

    menu resize

--- app/src/main/java/com/thesisit/flippino/ndk_story_p2.java
+++ app/src/main/java/com/thesisit/flippino/ndk_story_p2.java
@@ -43,7 +43,6 @@ public class ndk_story_p2 extends AppCompatActivity {
 
         mVisible = true;
         mControlsView = findViewById(R.id.fullscreen_content_controls);
-        mContentView = findViewById(R.id.fullscreen_content);
 
 
         // Set up the user interaction to manually show or hide the system UI.
@@ -57,7 +56,6 @@ public class ndk_story_p2 extends AppCompatActivity {
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
     }
 
     @Override
