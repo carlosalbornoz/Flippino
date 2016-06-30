d7e06956423ebac1f58164c94f73b622c680b7e1
Alexandria Ramos <alexandriamei_ramos@yahoo.com>
2016-07-01 03:00:47 +0800

    01.07.2016 - Revised scripts.xml - Added Phonograms screens for: H, N, L - Added Incomplete Phonogram screens for: M (missing Mo assets) - revised values.xml

--- app/src/main/java/com/thesisit/flippino/FlippinoStart.java
+++ app/src/main/java/com/thesisit/flippino/FlippinoStart.java
@@ -1,27 +1,30 @@
 package com.thesisit.flippino;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
+import android.view.View;
 
 public class FlippinoStart extends AppCompatActivity {
 
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
+    MediaPlayer mySound;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_gpr_intro_gu);
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
-
+        setContentView(R.layout.activity_flippino_start);
+        mySound = MediaPlayer.create(this, R.raw.youmakemesmile);
+        mySound.start();
     }
 
+    public void startMagsimula(View view) {
+        Intent intent = new Intent(this, NewUser.class);
+        startActivity(intent);
+    }
+
+    public void startMagpatuloy(View view) {
+        Intent intent = new Intent(this, UserLogin.class);
+        startActivity(intent);
+    }
 }
